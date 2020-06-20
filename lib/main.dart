d765ea01d302e1394efd4bed0cf8c4a8eedc35ed
尹恒 <yinheng@yinhengdeMacBook-Pro.local>
2020-06-20 11:27:37 +0800

    lib

--- lib/main.dart
+++ lib/main.dart
@@ -8,102 +8,198 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Welcome to flutter',
+      title: 'Shopping App',
       home: Scaffold(
         appBar: AppBar(
-          title: Text('Welcome to flutter'),
+          title: Text('Shopping App')
         ),
-        body: MyHomePage()
-
-        // Container(
-        //   child: Row(
-        //     children: <Widget>[
-        //       IconButton(
-        //         icon: Icon(Icons.search), 
-        //         tooltip: 'search',
-        //         onPressed: () {
-        //           print('search');
-        //         }
-        //       ),
-        //       IconButton(
-        //         icon: Icon(Icons.menu), 
-        //         tooltip: 'menu',
-        //         onPressed: null
-        //       ),
-        //       IconButton(
-        //         icon: Icon(Icons.home), 
-        //         tooltip: 'home',
-        //         onPressed: null
-        //       ),
-        //       GestureDetector(
-        //         onTap: () {
-        //           print('sb');
-        //         },
-        //         onDoubleTap: () {
-        //           print('sb 2');
-        //         },
-        //         child: Container(
-        //           width: 200,
-        //           height: 200,
-        //           margin: EdgeInsets.fromLTRB(20, 40, 0, 0),
-        //           color: Colors.blue,
-        //         ),
-        //       )
-        //     ],
-        //   ),
-        // ),
-
-        // body: Center(
-        //   child: Text(
-        //     'hello world', 
-        //     style: TextStyle(
-        //       color: Color.fromRGBO(255, 0, 0, 1), 
-        //       fontSize: 24, 
-        //       fontWeight: FontWeight.bold
-        //     ),
-        //   ),
-        // ),
-      )
+        body: ShoppingList(
+          products: <Product>[
+            Product('Eggs'),
+            Product('Flour'),
+            Product('Chocolate chips'),
+          ],
+        ),
+      ),
     );
   }
 }
 
-class MyHomePage extends StatefulWidget {
-  MyHomePage({Key key}) : super(key: key);
-
-  @override
-  _MyHomePage createState() => _MyHomePage();
+class Product {
+  const Product(this.name);
+  final String name;
 }
 
-class _MyHomePage extends State<MyHomePage> {
-  int _counter = 0;
-  
+typedef void CartChangedCallback(Product product, bool inCart);
 
-  void _incrementCounter() {
+class ShoppingList extends StatefulWidget {
+  ShoppingList({Key key, this.products}) : super(key: key);
+
+  final List<Product> products;
+
+  @override
+  _ShoppingListState createState() => _ShoppingListState();
+}
+
+class _ShoppingListState extends State<ShoppingList> {
+  Set<Product> _shoppingCart = <Product> {};
+
+  void _handleCartChanged(Product product, bool inCart) {
     setState(() {
-      _counter++;
+      if (inCart) {
+        _shoppingCart.add(product);
+      } else {
+        _shoppingCart.remove(product);
+      }
     });
   }
-  
+
   @override
   Widget build(BuildContext context) {
-    return Center(
-      child: Column(
-        mainAxisAlignment: MainAxisAlignment.center,
-        crossAxisAlignment: CrossAxisAlignment.center,
-        children: <Widget>[
-          Text(
-            'You have pushed the button this many times:',
-          ),
-          Text(
-            '$_counter'
-          ),
-          FloatingActionButton(onPressed: _incrementCounter, tooltip: 'Increment', child: Icon(Icons.add))
-        ],
-      )
+   return ListView(
+     padding: EdgeInsets.symmetric(vertical: 8.0),
+     children: widget.products.map((Product product) {
+       return ShoppingListItem(product, _shoppingCart.contains(product), _handleCartChanged);
+     }).toList(),
+   );
+  }
+}
+
+class ShoppingListItem extends StatelessWidget {
+  ShoppingListItem(Product product, this.inCart, this.onCartChanged): product = product, super(key: ObjectKey(product));
+
+  final Product product;
+  final bool inCart;
+  final CartChangedCallback onCartChanged;
+
+  Color _getColor(BuildContext context) {
+    return inCart ? Colors.black54 : Theme.of(context).primaryColor;
+  }
+
+  TextStyle _getTextStyle(BuildContext context) {
+    if (!inCart) return null;
+
+    return TextStyle(
+      color: Colors.black54,
+      decoration: TextDecoration.lineThrough,
+    );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return ListTile(
+      onTap: () {
+        onCartChanged(product, !inCart);
+      },
+      leading: CircleAvatar(
+        backgroundColor: _getColor(context),
+        child: Text(product.name[0]),
+      ),
+      title: Text(product.name, style: _getTextStyle(context)),
     );
   }
 }
+
+// class MyApp extends StatelessWidget {
+//   @override
+//   Widget build(BuildContext context) {
+//     return MaterialApp(
+//       title: 'Welcome to flutter',
+//       home: Scaffold(
+//         appBar: AppBar(
+//           title: Text('Welcome to flutter'),
+//         ),
+//         body: MyHomePage()
+
+//         // Container(
+//         //   child: Row(
+//         //     children: <Widget>[
+//         //       IconButton(
+//         //         icon: Icon(Icons.search), 
+//         //         tooltip: 'search',
+//         //         onPressed: () {
+//         //           print('search');
+//         //         }
+//         //       ),
+//         //       IconButton(
+//         //         icon: Icon(Icons.menu), 
+//         //         tooltip: 'menu',
+//         //         onPressed: null
+//         //       ),
+//         //       IconButton(
+//         //         icon: Icon(Icons.home), 
+//         //         tooltip: 'home',
+//         //         onPressed: null
+//         //       ),
+//         //       GestureDetector(
+//         //         onTap: () {
+//         //           print('sb');
+//         //         },
+//         //         onDoubleTap: () {
+//         //           print('sb 2');
+//         //         },
+//         //         child: Container(
+//         //           width: 200,
+//         //           height: 200,
+//         //           margin: EdgeInsets.fromLTRB(20, 40, 0, 0),
+//         //           color: Colors.blue,
+//         //         ),
+//         //       )
+//         //     ],
+//         //   ),
+//         // ),
+
+//         // body: Center(
+//         //   child: Text(
+//         //     'hello world', 
+//         //     style: TextStyle(
+//         //       color: Color.fromRGBO(255, 0, 0, 1), 
+//         //       fontSize: 24, 
+//         //       fontWeight: FontWeight.bold
+//         //     ),
+//         //   ),
+//         // ),
+//       )
+//     );
+//   }
+// }
+
+// class MyHomePage extends StatefulWidget {
+//   MyHomePage({Key key}) : super(key: key);
+
+//   @override
+//   _MyHomePage createState() => _MyHomePage();
+// }
+
+// class _MyHomePage extends State<MyHomePage> {
+//   int _counter = 0;
+  
+
+//   void _incrementCounter() {
+//     setState(() {
+//       _counter++;
+//     });
+//   }
+  
+//   @override
+//   Widget build(BuildContext context) {
+//     return Center(
+//       child: Column(
+//         mainAxisAlignment: MainAxisAlignment.center,
+//         crossAxisAlignment: CrossAxisAlignment.center,
+//         children: <Widget>[
+//           Text(
+//             'You have pushed the button this many times:',
+//           ),
+//           Text(
+//             '$_counter'
+//           ),
+//           FloatingActionButton(onPressed: _incrementCounter, tooltip: 'Increment', child: Icon(Icons.add))
+//         ],
+//       )
+//     );
+//   }
+// }
 
 // class MyApp extends StatelessWidget {
 //   // This widget is the root of your application.
@@ -128,91 +224,6 @@ class _MyHomePage extends State<MyHomePage> {
 //         visualDensity: VisualDensity.adaptivePlatformDensity,
 //       ),
 //       home: MyHomePage(title: 'Flutter Demo Home Page'),
-//     );
-//   }
-// }
-
-// class MyHomePage extends StatefulWidget {
-//   MyHomePage({Key key, this.title}) : super(key: key);
-
-//   // This widget is the home page of your application. It is stateful, meaning
-//   // that it has a State object (defined below) that contains fields that affect
-//   // how it looks.
-
-//   // This class is the configuration for the state. It holds the values (in this
-//   // case the title) provided by the parent (in this case the App widget) and
-//   // used by the build method of the State. Fields in a Widget subclass are
-//   // always marked "final".
-
-//   final String title;
-
-//   @override
-//   _MyHomePageState createState() => _MyHomePageState();
-// }
-
-// class _MyHomePageState extends State<MyHomePage> {
-//   int _counter = 0;
-
-//   void _incrementCounter() {
-//     setState(() {
-//       // This call to setState tells the Flutter framework that something has
-//       // changed in this State, which causes it to rerun the build method below
-//       // so that the display can reflect the updated values. If we changed
-//       // _counter without calling setState(), then the build method would not be
-//       // called again, and so nothing would appear to happen.
-//       _counter++;
-//     });
-//   }
-
-//   @override
-//   Widget build(BuildContext context) {
-//     // This method is rerun every time setState is called, for instance as done
-//     // by the _incrementCounter method above.
-//     //
-//     // The Flutter framework has been optimized to make rerunning build methods
-//     // fast, so that you can just rebuild anything that needs updating rather
-//     // than having to individually change instances of widgets.
-//     return Scaffold(
-//       appBar: AppBar(
-//         // Here we take the value from the MyHomePage object that was created by
-//         // the App.build method, and use it to set our appbar title.
-//         title: Text(widget.title),
-//       ),
-//       body: Center(
-//         // Center is a layout widget. It takes a single child and positions it
-//         // in the middle of the parent.
-//         child: Column(
-//           // Column is also a layout widget. It takes a list of children and
-//           // arranges them vertically. By default, it sizes itself to fit its
-//           // children horizontally, and tries to be as tall as its parent.
-//           //
-//           // Invoke "debug painting" (press "p" in the console, choose the
-//           // "Toggle Debug Paint" action from the Flutter Inspector in Android
-//           // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
-//           // to see the wireframe for each widget.
-//           //
-//           // Column has various properties to control how it sizes itself and
-//           // how it positions its children. Here we use mainAxisAlignment to
-//           // center the children vertically; the main axis here is the vertical
-//           // axis because Columns are vertical (the cross axis would be
-//           // horizontal).
-//           mainAxisAlignment: MainAxisAlignment.center,
-//           children: <Widget>[
-//             Text(
-//               'You have pushed the button this many times:',
-//             ),
-//             Text(
-//               '$_counter',
-//               style: Theme.of(context).textTheme.headline4,
-//             ),
-//           ],
-//         ),
-//       ),
-//       floatingActionButton: FloatingActionButton(
-//         onPressed: _incrementCounter,
-//         tooltip: 'Increment',
-//         child: Icon(Icons.add),
-//       ), // This trailing comma makes auto-formatting nicer for build methods.
 //     );
 //   }
 // }
